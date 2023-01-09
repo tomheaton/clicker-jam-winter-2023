9a59693122351f61b2d6823a53cf8d8a6b1c95f8
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-09 19:26:50 +0000

    move to cra

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/index.css";
+
+const App: React.FC = () => {
+  return (
+    <div className={"bg-black h-screen flex items-center justify-center"}>
+      <h1 className={"text-white font-bold text-5xl"}>clicker-jam-2023</h1>
+    </div>
+  );
+};
+
+export default App;
