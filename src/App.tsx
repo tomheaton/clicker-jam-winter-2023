3802bb31ffdcc574620861b83f7394ac64e47b7b
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-09 21:51:41 +0000

    music

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { useState } from "react";
 import Sprite from "./components/sprite";
 
 const App: React.FC = () => {
+  const [start, setStart] = useState<boolean>(false);
+
+  const handleStart = () => {
+    setStart(true);
+    new Audio("walking_in_the_highlands_1.mp3").play();
+  };
+
   return (
     <div
       className={"bg-black h-screen flex flex-col items-center justify-center"}
     >
       <h1 className={"mt-2 text-white font-bold text-5xl"}>clicker-jam-2023</h1>
       <br />
-      <Canvas>
-        <ambientLight />
-        {/* <Circle /> */}
-        <Sprite textureName={"megaman.png"}/>
-      </Canvas>
+      {start ? (
+        // TODO: extract to GameCanvas component
+        <Canvas>
+          <ambientLight />
+          {/* <Circle /> */}
+          <Sprite textureName={"megaman.png"} />
+        </Canvas>
+      ) : (
+        <button
+          onClick={handleStart}
+          className={
+            "px-4 py-2 text-white font-bold rounded-lg bg-blue-500 hover:bg-blue-600"
+          }
+        >
+          start game
+        </button>
+      )}
     </div>
   );
 };
