3b6ff2cf99a806bd3ba96011e94d6083142eb249
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-09 22:02:26 +0000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         <Canvas>
           <ambientLight />
           {/* <Circle /> */}
-          <Sprite textureName={"megaman.png"} />
+          <Sprite textureName={"mojito.png"} />
         </Canvas>
       ) : (
         <button
