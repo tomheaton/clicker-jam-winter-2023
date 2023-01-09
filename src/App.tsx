d59de2de0b19e3137e52e6b8a862b5d68ff95ca3
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-09 19:47:52 +0000

    canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import Circle from "./components/circle";
+import { Canvas } from "@react-three/fiber";
 import React from "react";
-import "./styles/index.css";
 
 const App: React.FC = () => {
   return (
-    <div className={"bg-black h-screen flex items-center justify-center"}>
-      <h1 className={"text-white font-bold text-5xl"}>clicker-jam-2023</h1>
+    <div
+      className={"bg-black h-screen flex flex-col items-center justify-center"}
+    >
+      <h1 className={"mt-2 text-white font-bold text-5xl"}>clicker-jam-2023</h1>
+      <br />
+      <Canvas>
+        <ambientLight />
+        <Circle />
+      </Canvas>
     </div>
   );
 };
---
+++ src/components/circle.tsx
@@ -0,0 +1,26 @@
+import * as THREE from "three";
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+const Circle: React.FC = (props: ThreeElements["mesh"]) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 0.5 : 0.25}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <circleGeometry args={[5, 32]}/>
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+export default Circle;
