5faf0a8faf11877412a51e63325028511ffae3db
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-13 05:23:37 +0000

    data work

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { INGREDIENTS } from "../data";
+
 export type Ingredient = {
   name: string;
   texture: string;
@@ -5,11 +7,19 @@ export type Ingredient = {
   upgradeDescriptions: string[];
 };
 
+// type IngredientName = keyof typeof INGREDIENTS[number]["name"];
+
 export type Drink = {
   name: string;
   texture: string;
   cooldown: number;
+  // ingredients: IngredientName[];
+  ingredients: string[];
 };
+
+// TODO: @tomheaton
+// export type Planet = typeof PLANETS[number];
+// export type Planet = string;
 
 // TODO: @Tufty62
 export type Item = {};
@@ -18,4 +28,6 @@ export type Data = {
   ingredients: Ingredient[];
   drinks: Drink[];
   items: Item[];
+  // TODO: @tomheaton
+  // planets: Planet[];
 };
