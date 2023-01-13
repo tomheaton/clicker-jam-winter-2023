9c5110b27c59529752c0b38b7a8533069ce723e9
Gonkie P <cryusdev@gmail.com>
2023-01-13 03:57:25 +0000

    tom look at this sliding menu thing

--- src/components/sliding_menu.tsx
+++ src/components/sliding_menu.tsx
@@ -3,26 +3,14 @@ import { useState } from "react";
 const SlideMenu: React.FC<{
 }> = ({}) => {
 
-  const [visible, setVisible] = useState<boolean>(false);
-
-        // className={"bg-green-500 fixed left-o top-0 w-screen h-screen" + 
-        //   (visible ? "transform: translate3d(100vw, 0, 0) " : "transform: translate3d(-100vw, 0, 0) z-[1]")} 
-
-  const handleClick = () => {
-    // setVisible(true);
-    setVisible((t) => (!t));
-    console.log(visible);
-  }
-
   return (
     <div>
       <div
-        className={"fixed left-o top-0 w-screen h-screen z-[1]" + 
-          (visible ? "bg-red-500" : "bg-green-500")} 
+        className={"scale-0 bg-green-500 fixed left-o top-0 w-screen h-screen z-[1] peer-hover:bg-red-500"}
       >
       </div>
-      <button onClick={() => handleClick()} className={"z-[10] absolute"}>
-        Open menu {visible.toString()}
+      <button className={"z-[10] absolute peer left-[200px] top-[200px]"}>
+        Open menu
       </button>
     </div>
   );
