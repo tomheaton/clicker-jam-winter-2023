29a5a2410d635e17468718736befe963e8da5bfc
Gonkie P <cryusdev@gmail.com>
2023-01-13 05:46:54 +0000

    fixed slider bug, removed option to specify width

--- src/components/slide_menu.tsx
+++ src/components/slide_menu.tsx
@@ -3,14 +3,10 @@ const SlideMenu: React.FC<{
   children: React.ReactNode;
 }> = ({ fromLeft, children }) => {
 
-  // NOTE: Change this to something like '1/2' or whatever fractino you want to change how far
-  // into the middle of the screen it goes (1/2 being going up to half)
-  const widthSize = "1/3";
-
-  const menuClassNameStr = `bg-green-500 z-[1] top-2 h-2/3 ease-in-out duration-300 fixed w-${widthSize}
+  const menuClassNameStr = `bg-green-500 z-[1] top-2 h-2/3 ease-in-out duration-300 fixed w-1/3
     ${fromLeft ? 
-      `-left-${widthSize} hover:left-0` : /*  left  */
-      `-right-${widthSize} hover:right-0`   /*  right */
+      `-left-1/3 hover:left-0` : /*  left  */
+      `-right-1/3 hover:right-0`   /*  right */
     }`;
 
   const buttonClassNameStr = `top-0 absolute ${fromLeft ? "left-full " : "right-full "}`;
