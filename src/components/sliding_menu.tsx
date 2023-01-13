c95de83cbbeda42ebbd966fc3418164c0c1b99d3
Gonkie P <cryusdev@gmail.com>
2023-01-13 03:27:34 +0000

    more work on sliding menu

--- src/components/sliding_menu.tsx
+++ src/components/sliding_menu.tsx
@@ -5,14 +5,25 @@ const SlideMenu: React.FC<{
 
   const [visible, setVisible] = useState<boolean>(false);
 
+        // className={"bg-green-500 fixed left-o top-0 w-screen h-screen" + 
+        //   (visible ? "transform: translate3d(100vw, 0, 0) " : "transform: translate3d(-100vw, 0, 0) z-[1]")} 
+
+  const handleClick = () => {
+    // setVisible(true);
+    setVisible((t) => (!t));
+    console.log(visible);
+  }
+
   return (
     <div>
       <div
-        className={"bg-red-500 fixed left-0 left-0 width-[100vw] height-[100vh]" + 
-          (visible ? "transform: translate3d(100vw, 0, 0) " : "transform: translate3d(-100vw, 0, 0) ")} 
+        className={"fixed left-o top-0 w-screen h-screen z-[1]" + 
+          (visible ? "bg-red-500" : "bg-green-500")} 
       >
       </div>
-      <button onClick={() => setVisible(true)}>Open menu</button>
+      <button onClick={() => handleClick()} className={"z-[10] absolute"}>
+        Open menu {visible.toString()}
+      </button>
     </div>
   );
 };
