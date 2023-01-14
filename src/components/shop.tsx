01adf6b16199bc3edf449b35fb651bd8cb5d9b5c
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-14 16:32:09 +0000

    shop work

--- src/components/shop.tsx
+++ src/components/shop.tsx
@@ -11,8 +11,8 @@ const Shop: React.FC<Props> = ({
   const [tab, setTab] = useState<"tab1" | "tab2" | "tab3">("tab1");
 
   return (
-    <div className="bg-white w-full">
-      <div className={"space-x-2 p-2"}>
+    <div className={"p-10"}>
+      <div className={"p-2 flex justify-evenly"}>
         <button className={"btn-blue"} onClick={() => setTab("tab1")}>
           Bar Upgrades
         </button>
@@ -30,30 +30,32 @@ const Shop: React.FC<Props> = ({
             <p>Go to Rocket Section</p>
           </div>
         )}
-        <div>
-          {tab == "tab2" && (
-            <div>
-              <p>NO</p>
-            </div>
-          )}
-        </div>
-        <div>
-          {tab == "tab3" && (
-            <>
-              <img
-                style={{
-                  imageRendering: "pixelated",
-                }}
-                className={"w-[100px] h-[100px]"}
-                src={`assets/drinks/moojito_1.png`}
-              ></img>
-              <p>
-                Item Name: {name} Item Cost: {itemCost} Item Description:{" "}
-                {itemDescription}
-              </p>
-            </>
-          )}
-        </div>
+
+        {tab == "tab2" && (
+          <div>
+            <p>NO</p>
+          </div>
+        )}
+
+        {tab == "tab3" && (
+          <div>
+            <img
+              style={{
+                imageRendering: "pixelated",
+              }}
+              className={"w-[100px] h-[100px]"}
+              src={`assets/drinks/moojito_1.png`}
+              alt={`Moojito sprite`}
+            />
+            <p>
+              Name: {name}
+              <br />
+              Cost: ${itemCost}
+              <br />
+              Description: {itemDescription}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
