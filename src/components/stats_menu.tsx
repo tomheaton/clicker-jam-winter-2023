43f5abfa265b16de9818f86eea0ccb8575887093
Thomas Heaton <thomas.m.heaton@gmail.com>
2023-01-14 00:06:22 +0000

    fix data

--- src/components/stats_menu.tsx
+++ src/components/stats_menu.tsx
@@ -1,32 +1,26 @@
 import { useContext } from "react";
-import {
-  GameDataContext,
-} from "../hooks/game_data";
+import { GameDataContext } from "../hooks/game_data";
 
 const StatsMenu: React.FC = () => {
   const gameData = useContext(GameDataContext);
 
   return (
     <div className={""}>
-      <h1 className={"text-center p-7"}>Stats</h1>  
+      <h1 className={"text-center p-7"}>Stats</h1>
       <div className={"p-7"}>
+        <p className={""}>Money: ${JSON.stringify(gameData!.money, null, 2)}</p>
 
         <p className={""}>
-          Money: ${JSON.stringify(gameData.money, null, 2)}
+          Drink selling price: {JSON.stringify(gameData!.drinkPrice, null, 2)}
         </p>
 
         <p className={""}>
-          Drink selling price: {JSON.stringify(gameData.drinkPrice, null, 2)}
+          Drinks/click: {JSON.stringify(gameData!.drinksPerClick, null, 2)}
         </p>
 
         <p className={""}>
-          Drinks/click: {JSON.stringify(gameData.drinksPerClick, null, 2)}
+          Drinks/s: {JSON.stringify(gameData!.drinksPerSecond, null, 2)}
         </p>
-
-        <p className={""}>
-          Drinks/s: {JSON.stringify(gameData.drinksPerSecond, null, 2)}
-        </p>
-
       </div>
     </div>
   );
